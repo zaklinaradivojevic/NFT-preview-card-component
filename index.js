44c2ea5b03ebbfc484165c21af5616d6b6de064f
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-11-20 14:59:19 +0100

    finishing styles

---
+++ index.js
@@ -0,0 +1,9 @@
+function showImg(){
+    document.getElementById("card_view").style.display="block";
+    document.getElementById("view").style.display="block";
+}
+ function hideImg(){
+    document.getElementById("card_view").style.display="none";
+    document.getElementById("view").style.display="none";
+
+ }
